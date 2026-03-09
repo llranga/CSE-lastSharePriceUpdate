bad86624500083275eee0b2679977da072a73d5f
llranga <jayasinghellr@gmail.com>
2026-03-09 19:55:34 +0300

    time changed

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def main():
     scheduler.add_job (update,
                       'cron',
                       day_of_week='mon-fri',
-                      hour='9-15',
+                      hour='23-15',
                       minute=0 
                       )
     scheduler.start()
