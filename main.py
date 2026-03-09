298dfcf052405d928ca9498dd2afcdc9207a65fa
llranga <jayasinghellr@gmail.com>
2026-03-09 05:45:28 +0300

    schedular added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 from localtime import Localtime
 from cse import CSE
 from telegrammessage import TelegramMessage 
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # open existing google sheet workbook
 scope = ["https://www.googleapis.com/auth/spreadsheets"]
@@ -23,35 +24,53 @@ workbook = client.open_by_key(sheet_id)
 #define variables
 banks = ["COMB.N0000","SEYB.N0000","SAMP.N0000","HNB.N0000","DIPD.N0000","TJL.N0000","AHUN.N0000","SERV.N0000"]
 bank_names = ["Commercial","Seylan","Sampath","HNB","Dipped Product","TeeJay","Aitken","Kingsbury"]
-last_trade_price=[]
-price_to_book_value=[]
 
-# create  my cse with known company code and names
-my_cse = CSE(banks,bank_names)
-last_trade_price = my_cse.get_last_trade_price()
 
-#create telegram message instance
-my_chat = TelegramMessage(bot_token,chat_id)
+def update():
+    last_trade_price=[]
+    price_to_book_value=[]
 
-#open the "watchlist" sheet of google sheet workbook
-sheet = workbook.worksheet('watchlist')
-#update cells
-for i in range (0,7,1):
-    sheet.update_cell(i+2,6,last_trade_price[i])
-# retrieve price to book value from google sheet
-for row in range (2,9,1):
-    price_to_book_value.append(sheet.cell(row,9).value)
-#print message if any company reaches PBV less than 0.81
-for i in range (0,7,1):
-    if float(price_to_book_value[i]) < 0.81 :
-        print(f"Buying target reach {bank_names[i]} @ {last_trade_price[i]}")
-        # send telegram message
-        my_chat.send_message(f"Buying target reach {bank_names[i]} @ {last_trade_price[i]}")
+    # create  my cse with known company code and names
+    my_cse = CSE(banks,bank_names)
+    last_trade_price = my_cse.get_last_trade_price()
 
-# get current time
-colombo_time = Localtime("Asia/Colombo").get_local_time()
-#update last update date time in the sheet
-sheet.update_cell(15,2,colombo_time)
+    #create telegram message instance
+    my_chat = TelegramMessage(bot_token,chat_id)
+
+    #open the "watchlist" sheet of google sheet workbook
+    sheet = workbook.worksheet('watchlist')
+    #update cells
+    for i in range (0,7,1):
+        sheet.update_cell(i+2,6,last_trade_price[i])
+    # retrieve price to book value from google sheet
+    for row in range (2,9,1):
+        price_to_book_value.append(sheet.cell(row,9).value)
+    #print message if any company reaches PBV less than 0.81
+    for i in range (0,7,1):
+        if float(price_to_book_value[i]) < 0.81 :
+            print(f"Buying target reach {bank_names[i]} @ {last_trade_price[i]}")
+            # send telegram message
+            my_chat.send_message(f"Buying target reach {bank_names[i]} @ {last_trade_price[i]}")
+
+    # get current time
+    colombo_time = Localtime("Asia/Colombo").get_local_time()
+    #update last update date time in the sheet
+    sheet.update_cell(15,2,colombo_time)
+
+def main():
+    scheduler = BlockingScheduler()
+    scheduler.add_job (update,
+                      'cron',
+                      day_of_week='mon-fri',
+                      hour='5-15',
+                      minute=0 
+                      )
+    scheduler.start()
+ 
+
+if __name__ == "__main__":
+    main()
+
 
 
 
